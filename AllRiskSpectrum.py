3cb0dc6d2957ee968cb92846cd9448fd21c40ecd
vachirakorn.R <vachirakorn.rungsikawanich@thomsonreuters.com>
2019-04-05 20:02:37 +0700

    add risk reader (command_code=13)

--- AllRiskSpectrum.py
+++ AllRiskSpectrum.py
@@ -5,11 +5,13 @@ from pandas import ExcelWriter
 
 headers = {
     # Request headers
-    'Ocp-Apim-Subscription-Key': 'xxxx' # Don't forget to put your keys in xxxx
+    'Ocp-Apim-Subscription-Key': '1a64accca0e0405e98ae04763d5f4eda' # Don't forget to put your keys in xxxx
 }
 
 from ratelimit import *
 import requests
+
+command_code = 13
 
 # 1 วิ เรียกได้ 5 ครั้ง
 class RateLimiter:
@@ -20,6 +22,8 @@ class RateLimiter:
     def call_get_api(self, url):
         response = requests.get(url, headers=headers)
 
+        if response.status_code == 204:
+            return response
         if response.status_code != 200:
             print('Cannot call API: {}'.format(response.status_code))
         return response
@@ -40,11 +44,12 @@ print(f'There are in total {len(all_funds.index)} funds')
 print(f'Estimate time to complete: {len(all_funds.index) / 4} seconds')
 
 # Create a place to hold risk_spectrum
-riskDF = pd.DataFrame(columns=['proj_id', 'risk_spectrum'])
+riskDF = pd.DataFrame(columns=['proj_id'])
 # Just something to track the progress
 count = 0
 progressstep = 10
 itr = 0
+
 for proj_id in all_funds.proj_id:
     count += 1
     if(count > (itr*progressstep) * len(all_funds.index) / 100) :
@@ -52,9 +57,20 @@ for proj_id in all_funds.proj_id:
         print(f'Estimate time to complete: {len(all_funds.index - count) / 4} seconds')
         itr += 1
 
-    req = limiter.call_get_api(url=f'https://api.sec.or.th/FundFactsheet/fund/{proj_id}/suitability')
-    temp_data = json.loads(req.content)
-    riskDF = riskDF.append([{'proj_id': proj_id, 'risk_spectrum': temp_data.get('risk_spectrum')}], ignore_index=True)
+    if command_code == 12:
+        res = limiter.call_get_api(url=f'https://api.sec.or.th/FundFactsheet/fund/{proj_id}/suitability')
+        temp_data = json.loads(res.content)
+        riskDF = riskDF.append([{'proj_id': proj_id, 'risk_spectrum': temp_data.get('risk_spectrum')}],
+                               ignore_index=True)
+    if command_code == 13:
+        res = limiter.call_get_api(url=f'https://api.sec.or.th/FundFactsheet/fund/{proj_id}/risk')
+        # read only not null
+        if res.status_code == 200:
+            items = json.loads(res.content)
+            for item in items:
+                riskDF = riskDF.append([{'proj_id': proj_id, 'group_code_desc': item.get('group_code_desc'), 'code_desc': item.get('code_desc'), 'last_upd_date': item.get('last_upd_date')}], ignore_index=True)
+    if len(riskDF) > 100:
+        break
 
 all_funds = all_funds.merge(riskDF)
 all_funds = all_funds.set_index('proj_id')
